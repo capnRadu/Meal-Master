8a96309baa6fc4ed5abefc9671b0db537c9c4c0a
Radu <aliment4476@gmail.com>
2024-05-31 14:45:26 +0300

    Fixed interactable bug
    Interactable would be picked up even after changing player destination

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -35,6 +35,10 @@ public class PlayerController : MonoBehaviour
                 {
                     interactable = interactableObject;
                 }
+                else
+                {
+                    interactable = null;
+                }
             }
         }
 
